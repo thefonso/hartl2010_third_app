c1108a951b8991526727ffbc3d4b8616cc61adfd
Alfonso <alephmage@gmail.com>
2011-11-21 19:11:39 -0600

    damn hartl chap 10 is broken this code is last green position at video 1002 22:13mins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   
   # before_filter :authenticate, :only => [:edit, :update]
-
+  
+  
   def show
     @user = User.find(params[:id])
     @title = @user.name
@@ -30,19 +31,19 @@ class UsersController < ApplicationController
   end
   
   def update
-    @user =User.find(params[:id])
-    if @user.update_attributes(params[:user])
-      redirect_to @user, :flash => {:success => "Profile updated."}
-    else
-      @title = "Edit user"
-      render 'edit'
-    end
-  end
+     @user = User.find(params[:id])
+     if @user.update_attributes(params[:user])
+       redirect_to @user, :flash => {:success => "Profile updated."}
+     else
+       @title = "Edit user"
+       render 'edit'
+     end
+   end
 
-  # private
-  # 
-  #    def authenticate
-  #      redirect_to signin_path unless signed_in?
-  #    end
+  private
+  
+  # def authenticate
+  #      deny_access unless signed_in?
+  #   end
 
 end
