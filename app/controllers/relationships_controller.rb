455f0321702dfd3921d65d3416abb3034781237d
Alfonso <alephmage@gmail.com>
2011-12-01 14:01:24 -0600

    CHAPTER 12 COMPLETE - Made a working feed

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,13 +2,21 @@ class RelationshipsController < ApplicationController
   before_filter :authenticate
   
   def create
-    user = User.find(params[:relationship][:followed_id])
-    current_user.follow!(user)
-    redirect_to user
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow!(@user)
+    respond_to do |format|
+      format.html { redirect_to @user}
+      format.js
+    end
   end
   
   def destroy
-    relationship = Relationship.find(params[:id]).destroy
-    redirect_to relationship.followed
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow!(@user)
+    #redirect_to relationship.followed
+    respond_to do |format|
+      format.html { redirect_to @user}
+      format.js
+    end
   end
 end
